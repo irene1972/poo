c9a3821fb3d1a95fa155936da0d29c81487146c2
Irene <ireneog_72@hotmail.es>
2019-12-13 09:09:34 +0100

    Add info about Excepciones

---
+++ 12-errores/index.php
@@ -0,0 +1,34 @@
+<?php
+// Tratamiento de EXCEPCIONES (errores)
+
+// TRY/CATCH: Sirve para capturar excepciones en código susceptible a errores (nosotros vamos a forzar el error con el throw)
+try{
+
+  if( isset( $_GET['id'] ) ){
+    echo "<h1>El parámetro es: {$_GET['id']}</h1>";
+  }else{
+    // Creamos el objeto Exception que ya existe dentro del lenguage PHP
+    // ---> esto hace saltar un Error PHP ¿cómo tratarlo? Con un try/catch
+    throw new Exception('<br>Faltan parámetros por la URL'); 
+  }
+
+}catch(Exception $e){
+
+  echo "Ha habido un error: " . $e->getMessage();
+
+}finally{
+
+  //El finally es el trozo de código que se ejecuta SIEMPRE después del catch
+  echo "<br>Todo correcto";
+
+}
+
+
+
+
+//$persona = new Persona( 'María', 30, 'Palencia' );
+
+
+
+
+
